7174dde27f41162fb5f4e719ca4928c2e585b7fc
basic-app <dev@basic-app.com>
2020-01-12 00:10:14 +0300

    n/a

---
+++ Database/Seeds/PageSeeder.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author Basic App Dev Team <dev@basic-app.com>
+ * @license MIT
+ * @link http://basic-app.com
+ */
+namespace BasicApp\Page\Database\Seeds;
+
+use BasicApp\Site\Models\PageModel;
+
+class PageSeeder extends \BasicApp\Core\Seeder
+{
+
+    public function run()
+    {
+        if ($this->db->table('pages')->countAllResults() === 0)
+        {
+            PageModel::getPage('index', true, [
+                'page_name' => 'Index',
+                'page_text' => '<p>Index page text.</p>',
+                'page_published' => 1
+            ]);
+
+            PageModel::getPage('about', true, [
+                'page_name' => 'About',
+                'page_text' => '<p>About page text.</p>',
+                'page_published' => 1
+            ]);
+        }
+    }
+
+}
