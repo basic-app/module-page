2c42904bb82b4049d4e4aaca5c70dd35b4cdbd73
basic-app <dev@basic-app.com>
2020-01-11 18:25:35 +0300

    n/a

--- Views/Admin/Page/_common.php
+++ Views/Admin/Page/_common.php
@@ -2,6 +2,6 @@
 
 $this->data['title'] = t('admin', 'Pages');
 
-$this->data['mainMenu']['pages']['active'] = true;
+$this->data['mainMenu']['site']['items']['pages']['active'] = true;
 
 $this->data['breadcrumbs'][] = ['label' => $this->data['title'], 'url' => site_url('admin/page')];
