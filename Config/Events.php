c6db93d76d634c683a13dc5bb10e9cbd0dc2d028
basic-app <dev@basic-app.com>
2020-01-12 00:12:35 +0300

    n/a

--- Config/Events.php
+++ Config/Events.php
@@ -8,12 +8,15 @@ use BasicApp\Helpers\Url;
 use BasicApp\Admin\AdminEvents;
 use BasicApp\System\SystemEvents;
 use BasicApp\System\Events\SystemResetEvent;
+use BasicApp\System\Events\SystemSeedEvent;
 use BasicApp\Page\Database\Seeds\PageResetSeeder;
+use BasicApp\Page\Database\Seeds\PageSeeder;
 use Config\Database;
+use BasicApp\Page\Controllers\Admin\Page as PageController;
 
 AdminEvents::onMainMenu(function($event)
 {
-    if (BasicApp\Page\Controllers\Admin\Page::checkAccess())
+    if (PageController::checkAccess())
     {
         $event->items['site']['items']['pages'] = [
             'url'   => Url::createUrl('admin/page'),
@@ -27,4 +30,11 @@ SystemEvents::onReset(function(SystemResetEvent $event)
     $seeder = Database::seeder();
 
     $seeder->call(PageResetSeeder::class);
+});
+
+SystemEvents::onSeed(function(SystemSeedEvent $event)
+{
+    $seeder = Database::seeder();
+
+    $seeder->call(PageSeeder::class);
 });
