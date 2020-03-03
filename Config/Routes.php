0e0dc519c288147e25ac22e8bd52a146701af46a
basic-app <dev@basic-app.com>
2020-03-03 21:18:37 +0300

    n/a

--- Config/Routes.php
+++ Config/Routes.php
@@ -8,7 +8,7 @@ $routes->add('page/(:any)', '\BasicApp\Page\Controllers\Page::view/$1');
 $routes->add('{locale}/page/(:any)', '\BasicApp\Page\Controllers\Page::view/$1');
 
 $routes->add('admin/page', '\BasicApp\Page\Controllers\Admin\Page::index');
-$routes->add('admin/page/(:segment)', '\BasicApp\Page\Controllers\Admin\Page::$1');
-
 $routes->add('{locale}/admin/page', '\BasicApp\Page\Controllers\Admin\Page::index');
+
+$routes->add('admin/page/(:segment)', '\BasicApp\Page\Controllers\Admin\Page::$1');
 $routes->add('{locale}/admin/page/(:segment)', '\BasicApp\Page\Controllers\Admin\Page::$1');
